920c51b56aec149f45545ad401a9c46400182a6a
t-hullis <t.hullis@yahoo.com>
2023-03-28 12:01:37 +0000

    add src files for machine learning

---
+++ src/machine_learning/predictive_analysis_ui.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+def predict_sale_price(X_live, house_features, sale_price_pipeline):
+
+	# from live data, subset features related to this pipeline
+	X_live_sale_price = X_live.filter(house_features)
+
+	# predict
+	sale_price_prediction_proba = sale_price_pipeline.predict(X_live_sale_price)
+
+	# create a logic to display the results
+	proba = sale_price_prediction_proba
+	value = float(proba.round(1))
+	amount = '${:,.2f}'.format(value)
+	statement = (
+		f'### With the values given, we estimate this house to be worth {amount} '
+	)
+
+	st.write(statement)
+		
+	st.write(proba)
