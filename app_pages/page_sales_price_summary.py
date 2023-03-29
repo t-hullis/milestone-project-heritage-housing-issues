270558e85e1ffe528b2e35513899dceecff218ca
t-hullis <t.hullis@yahoo.com>
2023-03-29 13:33:35 +0000

    make change to page saes price summary

--- app_pages/page_sales_price_summary.py
+++ app_pages/page_sales_price_summary.py
@@ -110,7 +110,7 @@ def plot_numerical(df, col, target_var):
     st.pyplot(fig)
 
 
-def correlation_to_sale_price(df_eda, vars_to_study):
+def correlation_to_sale_price(df, vars_to_study):
     target_var = 'SalePrice'
     for col in ['GarageArea', 'GrLivArea', 'OverallQual', 'TotalBsmtSF', 'YearBuilt']:
         plot_numerical(df_eda, col, target_var)
