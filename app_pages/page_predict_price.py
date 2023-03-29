3d15f6ec595ca4c9cd752c2b0514debd1b010b72
t-hullis <t.hullis@yahoo.com>
2023-03-29 10:58:40 +0000

    change the columns in the predictor to match x_test

--- app_pages/page_predict_price.py
+++ app_pages/page_predict_price.py
@@ -126,16 +126,17 @@ def DrawInputsWidgets():
 			)
 	X_live[feature] = st_widget
 
-	with col6:
-		feature = "YearRemodAdd"
-		st_widget = st.number_input(
-			label= feature,
-			min_value= int(df[feature].min()*percentageMin), 
-			max_value= date.today().year,
-			value= int(df[feature].median()), 
-            step= 1
-			)
-	X_live[feature] = st_widget
+
+	# with col6:
+	# 	feature = "YearRemodAdd"
+	# 	st_widget = st.number_input(
+	# 		label= feature,
+	# 		min_value= int(df[feature].min()*percentageMin), 
+	# 		max_value= date.today().year,
+	# 		value= int(df[feature].median()), 
+    #         step= 1
+	# 		)
+	# X_live[feature] = st_widget
 
 
 	# st.write(X_live)
